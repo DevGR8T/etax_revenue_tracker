a600e9b138dcdcf9f136ea48b24568968e305072
DevGR8T <greatchikezie@gmail.com>
2026-04-24 12:53:17 +0100

    feat(core): add nigerian states and levy types constants

---
+++ lib/core/constants/levy_types.dart
@@ -0,0 +1,14 @@
+/// Tax levy types and assessment years.
+abstract final class LevyTypes {
+  static const List<String> all = [
+    'Property Tax',
+    'Business Levy',
+    'Vehicle Tax',
+    'Land Use Charge',
+    'Personal Income Tax',
+  ];
+
+  static const List<String> assessmentYears = [
+    '2020', '2021', '2022', '2023', '2024', '2025', '2026',
+  ];
+}
---
+++ lib/core/constants/nigerian_states.dart
@@ -0,0 +1,13 @@
+/// All 36 Nigerian states plus FCT.
+abstract final class NigerianStates {
+  static const List<String> all = [
+    'Abia', 'Adamawa', 'Akwa Ibom', 'Anambra', 'Bauchi',
+    'Bayelsa', 'Benue', 'Borno', 'Cross River', 'Delta',
+    'Ebonyi', 'Edo', 'Ekiti', 'Enugu', 'FCT (Abuja)',
+    'Gombe', 'Imo', 'Jigawa', 'Kaduna', 'Kano',
+    'Katsina', 'Kebbi', 'Kogi', 'Kwara', 'Lagos',
+    'Nasarawa', 'Niger', 'Ogun', 'Ondo', 'Osun',
+    'Oyo', 'Plateau', 'Rivers', 'Sokoto', 'Taraba',
+    'Yobe', 'Zamfara',
+  ];
+}

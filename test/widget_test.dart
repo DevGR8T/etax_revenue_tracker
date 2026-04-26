1eb109bfb83af3d62f1a992c95e02aa080f47efd
DevGR8T <greatchikezie@gmail.com>
2026-04-26 13:52:46 +0100

    chore: update default widget test from flutter create

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:etax_revenue_tracker/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const EtaxApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);

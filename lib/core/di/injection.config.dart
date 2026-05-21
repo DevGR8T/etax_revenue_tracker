0f53617246af7965feb87da7b277827c24a76746
DevGR8T <greatchikezie@gmail.com>
2026-05-21 01:16:59 +0100

    fix: register DashboardBloc with GetIt via build_runner

--- lib/core/di/injection.config.dart
+++ lib/core/di/injection.config.dart
@@ -43,12 +43,18 @@ import 'package:etax_revenue_tracker/features/auth/presentation/bloc/auth_bloc.d
     as _i802;
 import 'package:etax_revenue_tracker/features/auth/presentation/bloc/forgot_password_cubit.dart'
     as _i765;
+import 'package:etax_revenue_tracker/features/dashboard/data/datasources/dashboard_remote_datasource.dart'
+    as _i654;
+import 'package:etax_revenue_tracker/features/dashboard/data/repositories/dashboard_repository_impl.dart'
+    as _i357;
 import 'package:etax_revenue_tracker/features/dashboard/domain/repositories/dashboard_repository.dart'
     as _i519;
 import 'package:etax_revenue_tracker/features/dashboard/domain/usecases/get_dashboard_data_usecase.dart'
     as _i1025;
 import 'package:etax_revenue_tracker/features/dashboard/domain/usecases/refresh_dashboard_usecase.dart'
     as _i184;
+import 'package:etax_revenue_tracker/features/dashboard/presentation/bloc/dashboard_bloc.dart'
+    as _i959;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart' as _i558;
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
@@ -79,12 +85,6 @@ extension GetItInjectableX on _i174.GetIt {
     );
     gh.lazySingleton<_i510.NetworkInfo>(
       () => _i510.NetworkInfoImpl(gh<_i895.Connectivity>()),
-    );
-    gh.factory<_i184.RefreshDashboardUseCase>(
-      () => _i184.RefreshDashboardUseCase(gh<_i519.DashboardRepository>()),
-    );
-    gh.factory<_i1025.GetDashboardDataUseCase>(
-      () => _i1025.GetDashboardDataUseCase(gh<_i519.DashboardRepository>()),
     );
     gh.lazySingleton<_i1061.AuthRemoteDataSource>(
       () => _i1061.AuthRemoteDataSourceImpl(
@@ -126,12 +126,36 @@ extension GetItInjectableX on _i174.GetIt {
     gh.factory<_i765.ForgotPasswordCubit>(
       () => _i765.ForgotPasswordCubit(gh<_i472.SendResetLinkUseCase>()),
     );
+    gh.lazySingleton<_i654.DashboardRemoteDataSource>(
+      () => _i654.DashboardRemoteDataSourceImpl(
+        gh<_i361.Dio>(instanceName: 'dummyjson'),
+        gh<_i510.NetworkInfo>(),
+      ),
+    );
     gh.factory<_i802.AuthBloc>(
       () => _i802.AuthBloc(
         gh<_i698.LoginUseCase>(),
         gh<_i759.RegisterUseCase>(),
         gh<_i457.LogoutUseCase>(),
         gh<_i956.AuthRepository>(),
+      ),
+    );
+    gh.lazySingleton<_i519.DashboardRepository>(
+      () => _i357.DashboardRepositoryImpl(
+        gh<_i654.DashboardRemoteDataSource>(),
+        gh<_i269.SupabaseService>(),
+      ),
+    );
+    gh.factory<_i184.RefreshDashboardUseCase>(
+      () => _i184.RefreshDashboardUseCase(gh<_i519.DashboardRepository>()),
+    );
+    gh.factory<_i1025.GetDashboardDataUseCase>(
+      () => _i1025.GetDashboardDataUseCase(gh<_i519.DashboardRepository>()),
+    );
+    gh.factory<_i959.DashboardBloc>(
+      () => _i959.DashboardBloc(
+        gh<_i1025.GetDashboardDataUseCase>(),
+        gh<_i184.RefreshDashboardUseCase>(),
       ),
     );
     return this;
